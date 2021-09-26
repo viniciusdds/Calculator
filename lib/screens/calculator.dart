4b9d8dab5657b319f79ac4eaea1f520ddf41fc69
viniciusdds <viniciusdamasceno180@hotmail.com>
2021-09-25 21:32:05 -0300

    aula 455

---
+++ lib/screens/calculator.dart
@@ -0,0 +1,33 @@
+import 'package:calculator/components/display.dart';
+import 'package:calculator/components/keyboard.dart';
+import 'package:calculator/models/memory.dart';
+import 'package:flutter/material.dart';
+
+class Calculator extends StatefulWidget {
+
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+
+  final Memory memory = Memory();
+
+  _onPressed(String command){
+   setState(() {
+     memory.applyCommand(command);
+   });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Column(
+        children: [
+          Display(memory.value),
+          Keyboard(_onPressed),
+        ],
+      ),
+    );
+  }
+}
