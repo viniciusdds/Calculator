d639b6f1c9b66d20aa1dcd821f129dee12373120
viniciusdds <viniciusdamasceno180@hotmail.com>
2021-09-27 22:11:42 -0300

    Projeto finalizado

--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -2,6 +2,7 @@ import 'package:calculator/components/display.dart';
 import 'package:calculator/components/keyboard.dart';
 import 'package:calculator/models/memory.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Calculator extends StatefulWidget {
 
@@ -21,7 +22,12 @@ class _CalculatorState extends State<Calculator> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp
+    ]);
+
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Column(
         children: [
           Display(memory.value),
